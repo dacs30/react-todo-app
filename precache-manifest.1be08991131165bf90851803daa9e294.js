b056319bcd919ae1c7e689abaf7694ea440cf939
dacs2 <dacs2012@gmail.com>
2020-08-24 16:53:29 -0300

    Updates

---
+++ precache-manifest.1be08991131165bf90851803daa9e294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e03af5e18f0d1363e81a2065596e00",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "c37ca89a75895a88367b",
+    "url": "/react-todo-app/static/css/main.f7df9494.chunk.css"
+  },
+  {
+    "revision": "61ba654bfc8ca0ed8942",
+    "url": "/react-todo-app/static/js/2.9387b7c7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.9387b7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37ca89a75895a88367b",
+    "url": "/react-todo-app/static/js/main.cd3249c2.chunk.js"
+  },
+  {
+    "revision": "33037d1e99bc3f84b258",
+    "url": "/react-todo-app/static/js/runtime-main.d1a840a9.js"
+  }
+]);
